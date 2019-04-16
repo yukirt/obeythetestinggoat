8fa55ea3548a0ca3099152ac269f15f94aa4cd59
kirt <jkyu@gms.ndhu.edu.tw>
2019-04-16 15:21:23 +0800

    Test the list

--- function_test.py
+++ function_test.py
@@ -18,19 +18,15 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
-
-        inputbox.send_keys('buy peacock feathers')
-
+        #self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 
         self.fail('Finish the test!')
