e937307e9aa1760a3a78f066e4a5c4226a9a6ac7
kirt <jkyu@gms.ndhu.edu.tw>
2019-04-16 13:44:31 +0800

    Functional test now checks we can input a tido item

--- function_test.py
+++ function_test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     
@@ -12,9 +14,25 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+
+        inputbox.send_keys('buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: buy peacock feathers' for row in rows))
+
+
         self.fail('Finish the test!')
 
         #[...rest of comment as before]
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     unittest.main(warnings='ignore')
