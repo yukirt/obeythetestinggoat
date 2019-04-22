2a50b7e34b774dfe6b9839a5b7f69975bf32b75e
kirt-yu <jkyu@gms.ndhu.edu.tw>
2019-04-22 14:06:08 +0800

    Fix setting typo

--- superlist/settings.py
+++ superlist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r9!p$7a6^yq6!$3yx56oj))1i_pr+&!$%%^-ks29&=_+anulna'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-if 'DJANGO_DEBUG_FALES' in os.environ:
+if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     ALLOWED_HOSTS = [os.environ['SITENAME']]
