98dfcd73c8b88e9e04ba705a2ca226d28618c0e7
kirt-yu <jkyu@gms.ndhu.edu.tw>
2019-04-24 13:22:55 +0800

    Adjust new list view to do model validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 from lists.models import Item, List
+from django.utils.html import escape
 
 class HomePageTest(TestCase):
 
@@ -53,6 +54,18 @@ class NewListTests(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text':''})
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text':''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
 class NewItemTest(TestCase):
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
@@ -77,5 +90,5 @@ class NewItemTest(TestCase):
             f'/lists/{correct_list.id}/add_item',
             data={'item_text': 'A new item for an existing list'}
         )
-
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
