3d8957a655c4c9fdd19d85530a04661955b93027
kirt-yu <jkyu@gms.ndhu.edu.tw>
2019-04-25 17:08:23 +0800

    Rename all item input ids and names. still broken

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,7 @@ from lists.views import home_page
 from django.template.loader import render_to_string
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 class HomePageTest(TestCase):
 
@@ -93,12 +93,25 @@ class NewListTests(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'text':''})
-        self.assertEqual(response.status_code,200)
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/lists/new',data={'text': ''})
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    # def test_validation_errors_are_sent_back_to_home_page_template(self):
+    #     response = self.client.post('/lists/new', data={'text':''})
+    #     self.assertEqual(response.status_code,200)
+    #     self.assertTemplateUsed(response, 'home.html')
+    #     expected_error = escape(EMPTY_ITEM_ERROR)
+    #     self.assertContains(response, expected_error)
 
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'text':''})
