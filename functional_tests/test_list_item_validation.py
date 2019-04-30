d38edf24482f0ca823b7bb0dbea9be09711805b3
kirt-yu <jkyu@gms.ndhu.edu.tw>
2019-04-30 10:17:56 +0800

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.keys import Keys
+from django.core.exceptions import ValidationError
 from .base import FunctionTest
+from lists.models import List, Item
 
 class ItemValidationTest(FunctionTest):
 
@@ -41,3 +43,34 @@ class ItemValidationTest(FunctionTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_items(self):
+        #Edith goes to the home page and starts a new lin
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+
+        #She accidentally tries to enter a duplicate utem
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        
+        #She sees a helpful error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You've already got this in your list"
+        ))
+
+    def test_duplicate_item_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.creae(lost=list_, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean()
