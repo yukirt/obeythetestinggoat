3142ae0dd555b165b3ec2701e2e1b968f0b72e71
kirt-yu <jkyu@gms.ndhu.edu.tw>
2019-05-07 17:36:44 +0800

    Add send_login_email & login functions

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from accounts.models import Token
+from django.core.urlresolvers import reverse
+import sys
 
-# Create your views here.
+def send_login_email(request):
+    email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
+    send_mail(
+        'Your login link for Superlists',
+        message_body,
+        'noreply@superlists',
+        [email],
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    # messages.add_message(
+    #     request,
+    #     messages.SUCCESS,
+    #     "Check your email, we've sent you a link you can use to log in."
+    # )
+    return redirect('/')
+
+def login(request):
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
+    # print('login view', file=sys.stderr)
+    # uid = request.GET.get('uid')
+    # user = auth.authenticate(uid=uid)
+    # if user is not None:
+    #     auth.login(request, user)
+    return redirect('/')
