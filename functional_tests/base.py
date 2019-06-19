c4d0d7ac61d37274461f25a6140137b3ae0685c6
kirt <jkyu@gms.ndhu.edu.tw>
2019-06-19 16:44:49 +0800

    Chapter 20 end

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,6 +7,18 @@ from .server_tools import reset_database
 
 MAX_WAIT = 10
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
 class FunctionTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -29,6 +41,7 @@ class FunctionTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -45,6 +58,7 @@ class FunctionTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
 
+    @wait
     def wait_to_be_logged_in(self, email):
         self.wait_for(
             lambda: self.browser.find_element_by_link_text('Log out')
@@ -52,6 +66,8 @@ class FunctionTest(StaticLiveServerTestCase):
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
+    
+    @wait
     def wait_to_be_logged_out(self, email):
         self.wait_for(
             lambda: self.browser.find_element_by_name('email')
